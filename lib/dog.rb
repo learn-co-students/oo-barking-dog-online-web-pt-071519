791098f879a88b14406886b5f8c0f6471797c62b
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-24 12:01:14 -0700

    Added comments

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 class Dog
+  #writer
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
-
+  #reader
   def name
     @this_dogs_name
   end
@@ -13,6 +14,9 @@ class Dog
 
 end
 
+#create new instance
 fido = Dog.new
+#use method on line 3 to set object's name to fido
 fido.name = "Fido"
+#call method on line seven to call set name
 puts fido.name
