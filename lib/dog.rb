4529738d0ce71235e6d2236cce68f040afe54ed3
Paul Beckwith <303dog@gmail.com>
2019-08-04 17:40:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name
+  end 
+
+  def has_a_name(dog_name)
+    @new_dogs_name
+  end 
+    has_a_name 
+  
+  def bark 
+    puts "woof!"
+  end
+end
